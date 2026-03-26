0b33d176c732e50435018dac6709e1ee3599259f
hoaitrandev <155989483+hoaitrandev2003@users.noreply.github.com>
2026-03-26 09:27:19 +0700

    Add username column to users table

--- database/db_mini_bank_system.sql
+++ database/db_mini_bank_system.sql
@@ -9,6 +9,7 @@ CREATE TABLE roles (
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     full_name VARCHAR(150),
