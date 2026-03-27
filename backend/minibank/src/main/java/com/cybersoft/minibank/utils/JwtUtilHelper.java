f2086b4d489827c03dbf9dae26c36262153c8816
Hoai Tran <hoaitrandev@gmail.com>
2026-03-27 14:24:10 +0700

    fix bug error 403 and edit JWT filter (Spring  security)

--- backend/minibank/src/main/java/com/cybersoft/minibank/utils/JwtUtilHelper.java
+++ backend/minibank/src/main/java/com/cybersoft/minibank/utils/JwtUtilHelper.java
@@ -1,5 +1,6 @@
 package com.cybersoft.minibank.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtilHelper {
@@ -25,5 +27,36 @@ public class JwtUtilHelper {
 
         String jws = Jwts.builder().subject(data).expiration(expiryDate).signWith(key).compact();
         return jws;
+    }
+    // Lấy username từ token
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // Lấy expiration
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+    // Generic lấy claim
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+        final Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return resolver.apply(claims);
+    }
+
+    // Kiểm tra token hết hạn chưa
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    // Validate token
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
     }
 }
