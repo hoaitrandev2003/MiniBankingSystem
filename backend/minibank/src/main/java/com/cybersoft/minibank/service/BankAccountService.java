75a07177cd08c2080cdfeafc1ae6dd1529983bc2
Hoai Tran <hoaitrandev@gmail.com>
2026-03-28 12:21:51 +0700

    fix trnsferMoney git

--- backend/minibank/src/main/java/com/cybersoft/minibank/service/BankAccountService.java
+++ backend/minibank/src/main/java/com/cybersoft/minibank/service/BankAccountService.java
@@ -1,8 +1,11 @@
 package com.cybersoft.minibank.service;
 
+import com.cybersoft.minibank.dto.TransferRequestDTO;
+
 import java.math.BigDecimal;
 
 public interface BankAccountService {
     String deposit(String accountNumber, BigDecimal amount, String description);
     double getAccountBalance(String accountNumber);
+    void transferMoney(TransferRequestDTO request);
 }
